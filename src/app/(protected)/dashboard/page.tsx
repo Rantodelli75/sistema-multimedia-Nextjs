023568c77999613a282abf7a1dd23eff81c97c99
DePonteJuan <de.ponte.juan.jose@gmail.com>
2025-02-24 13:37:18 -0400

    redirect to admin

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "../../../../auth"
 import UserDashboardLayout from "@/components/features/dashboard/userDashboard"
 import LoadingSpinner from "@/components/features/auth/loading-spinner"
 import MusicContent from "@/components/features/dashboard/MusicContent"
+import { redirect } from "next/navigation"
 
 export default async function DasboardPage() {
     const session = await auth()
@@ -11,6 +12,10 @@ export default async function DasboardPage() {
   if (session === null) {
     return <LoadingSpinner shouldReturn={true} />
   }
+  if (session && session.user.role === "ADMIN") {
+    redirect("/admin/users")
+  }
+   
   return (
     <>
       <UserDashboardLayout session={session}>
